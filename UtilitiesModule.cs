bff1539cdee79cb82ff57adaade937126900ffad
TiagoRG <tiagorg.dll@outlook.com>
2020-04-21 15:57:40 +0100

    Added suggest command (need BIG upgrade)

--- UtilitiesModule.cs
+++ UtilitiesModule.cs
@@ -11,13 +11,14 @@ namespace Rendy
     public class UtilitiesModule : ModuleBase<SocketCommandContext>
     {
         [Command("tell")]
+        [RequireOwner]
         public async Task Tell(IUser user, [Remainder] string msg)
         {
             Embed embed = new EmbedBuilder()
                 .WithAuthor("Rendy")
                 .WithColor(Color.Green)
-                .WithDescription($"You got a message from {user.Username}!")
-                .WithTitle("New Message!")
+                .WithDescription($"You got a message from Rendy Owner!")
+                .WithTitle("TiagoRG")
                 .WithFooter($"Rendy{Const.copyright} by TiagoRG#8003")
                 .AddField("**Message Content**", msg)
                 .AddField("**Sent At**", Context.Message.Timestamp)
@@ -27,6 +28,28 @@ namespace Rendy
             await user.SendMessageAsync(embed: embed);
             var reply = await Context.Channel.SendMessageAsync("Message sent!");
             await Task.Delay(2000);
+            await reply.DeleteAsync();
+        }
+        [Command("suggest")]
+        public async Task Suggest([Remainder] string suggestion)
+        {
+            await Context.Message.DeleteAsync();
+            /*var newChannel = await Context.Guild.CreateTextChannelAsync("suggestions");*/
+
+            var suggestionEmbed = new EmbedBuilder()
+                .WithAuthor("Rendy")
+                .WithColor(Color.Green)
+                .WithDescription("We got a new suggestion for the server!")
+                .WithThumbnailUrl(Context.Message.Author.GetAvatarUrl())
+                .WithTitle(Context.Message.Author.Username)
+                .WithFooter(Const.embedFooter)
+                .AddField("Suggestion", suggestion)
+                .AddField("Sent At", $"{Context.Message.CreatedAt.Day}/{Context.Message.CreatedAt.Month}/{Context.Message.CreatedAt.Year}")
+                .Build();
+            /*await newChannel.SendMessageAsync(embed: suggestionEmbed);*/
+            await Context.Guild.Owner.SendMessageAsync(embed: suggestionEmbed);
+            var reply = await Context.Channel.SendMessageAsync("Your suggestion have been successfully sent!");
+            await Task.Delay(1000);
             await reply.DeleteAsync();
         }
     }
